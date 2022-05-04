a07785e90518658d722b7e7ff6fda49295ad737e
pratibha thapa <pratibha.thapa159@gmail.com>
2022-05-04 14:32:59 +0545

    first commit

---
+++ frontend/src/components/Background.tsx
@@ -0,0 +1,47 @@
+import {useState, useEffect} from "react"
+import { Image } from "@chakra-ui/react";
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+function Background() {
+  const { width, height } = useWindowDimensions();
+  const img = 'https://images.unsplash.com/photo-1622793139910-e261be0b30a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80';
+
+  return (
+    <Image
+      position="fixed"
+      top="0"
+      left="0"
+      bottom="0"
+      right="0"
+      zIndex="1"
+      src={img}
+      alt="bg"
+    />
+  );
+}
+
+export default Background;
